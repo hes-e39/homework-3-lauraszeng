ab2f70cf9890d1513df2fbe51d3d12fd396ead0d
lauraszeng <113085949+lauraszeng@users.noreply.github.com>
2024-10-02 00:50:00 -0400

    Update App.tsx
    app now spits out the entire color list

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import { useState } from 'react';
-import reactLogo from '/react.svg';
+import data from './colors.json';
+
+// create a type to define the shape of the complementary colors property in a colors.json element
+type CompColor = {
+    hex: string;
+    name: string;
+};
+
+// create a type to define the shape of the elements from colors.json
+type ColorJSON = {
+    hex: string;
+    name: string;
+    comp: Array<CompColor>;
+};
+
+// create a variable to store the array of colors, specify the array type as being ColorJSON
+const ColorList: ColorJSON[] = data;
+
+// function to create React components for each element in ColorList
+const ColorDiv = () => {
+    // use map to create a div component for each element
+    const colormap = ColorList.map(i => {
+        // create a constant to style the element's color using the hex property
+        const css_color: string = `#${i.hex}`;
+        // map out a list of comparable colors using the comp property
+        const comparable_colors = i.comp.map(i => {
+            const comp_css_color: string = `#${i.hex}`;
+            return (
+                <li key={i.name} style={{ color: comp_css_color }}>
+                    Hex: {i.hex}{' '}
+                </li>
+            );
+        });
+        return (
+            <div style={{ color: css_color }} key={i.hex}>
+                Hex: {i.hex} <br />
+                Name: {i.name} <br />
+                Comparable Colors: {comparable_colors} <br />
+            </div>
+        );
+    });
+    return <div>{colormap}</div>;
+};
 
 function App() {
-    const [count, setCount] = useState<number>(1);
-
+    console.info('hi hello');
+    console.info(ColorList);
     return (
-        <div className="flex flex-col items-center gap-12 pt-32">
-            <div>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="size-32" alt="React logo" />
-                </a>
-            </div>
-
-            <div className="text-3xl font-bold underline">E-39: Vite + React + TypeScript</div>
-            <div>
-                <button className="border-[0.5px] border-white p-2" onClick={() => setCount(count => count + 1)}>
-                    Click me: {count}
-                </button>
-            </div>
+        <div>
+            <ColorDiv />
         </div>
     );
 }
